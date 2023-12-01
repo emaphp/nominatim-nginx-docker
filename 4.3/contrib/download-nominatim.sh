b88bb9f5112f8149761350288ef9458240a0a079
Emmanuel Antico <emmanuel.antico@gmail.com>
2023-12-01 14:28:33 -0300

    update to nominatim 4.3.2

--- 4.3/contrib/download-nominatim.sh
+++ 4.3/contrib/download-nominatim.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ENV CURRENT_VERSION=1.0.0
-ENV NOMINATIM_VERSION=4.3.1
+ENV NOMINATIM_VERSION=4.3.2
 ARG USER_AGENT=emaphp/nominatim-nginx-docker:${CURRENT_VERSION}-${NOMINATIM_VERSION}
 
 curl -A $USER_AGENT https://nominatim.org/release/Nominatim-$NOMINATIM_VERSION.tar.bz2 -o nominatim.tar.bz2
